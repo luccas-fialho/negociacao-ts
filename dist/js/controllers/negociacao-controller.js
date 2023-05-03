e8553e69f1ceb4ee87a2819e60a47b4db9b54367
Luccas Fialho <luccas.flh@gmail.com>
2023-05-03 20:34:02 -0300

    Criando repo

---
+++ dist/js/controllers/negociacao-controller.js
@@ -0,0 +1,26 @@
+import { Negociacao } from "../models/negociacao.js";
+export class NegociacaoController {
+    constructor() {
+        this.inputData = document.querySelector('#data');
+        this.inputQuantidade = document.querySelector('#quantidade');
+        this.inputValor = document.querySelector('#valor');
+    }
+    adiciona() {
+        const negociacao = this.criaNegociacao();
+        console.log(negociacao);
+        this.limparFormulario();
+    }
+    criaNegociacao() {
+        const exp = /-/g; //Expressão regular que encontra todos os hifens
+        const date = new Date(this.inputData.value.replace(exp, ',')); //Substitui todos os hifens encontrados pela expressão regular pela virgula
+        const quantidade = parseInt(this.inputQuantidade.value);
+        const valor = parseFloat(this.inputValor.value);
+        return new Negociacao(date, quantidade, valor);
+    }
+    limparFormulario() {
+        this.inputData.value = '';
+        this.inputQuantidade.value = '';
+        this.inputValor.value = '';
+        this.inputData.focus();
+    }
+}
